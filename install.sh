d7b07516a4e45657fb07c69aafd459d9f59abd21
Wiryono Lauw <wiryonolau@gmail.com>
2018-06-19 10:47:27 +0700

    fix typo and install script

--- install.sh
+++ install.sh
@@ -1,31 +1,74 @@
 #!/usr/bin/env bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
+VERBOSE=""
+NOUPDATE=""
 
-#Install jq (bash json parser) library
-apt-get update
-apt-get install -y jq
+function usage
+{
+    echo "Usage: install.sh [-v|--verbose] [--noupdate]"
+    echo " -v | --verbose   : output verbose"
+    echo " --noupdate       : Don't run apt-get update"
+}
 
-#Put script in opt
-WORKDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-mkdir -p /opt/gputweak
-cp -r ${WORKDIR}/* /opt/gputweak
-rm -rf /usr/bin/gputweak
-ln -s /opt/gputweak/gputweak /usr/bin/gputweak
+function parse_args
+{
+    local _ARGS=()
 
-#Setup main xorg.conf
-if [[ ! -z "/etc/X11/xorg.conf" ]]; then
-    chattr -i /etc/X11/xorg.conf
-    cp /etc/X11/xorg.conf /etc/X11/xorg.conf.orig
-fi
-cp /opt/gputweak/config/xorg.conf /etc/X11/xorg.conf
-chattr +i /etc/X11/xorg.conf
+    while [ "$1" ]
+    do
+        case "$1" in
+            "-v" | "--verbose" )
+                VERBOSE=1
+            ;;
+            "-h" | "--help")
+                usage
+                exit 0
+                ;;
+            "--noupdate" )
+                NOUPDATE=1
+            ;;
+        esac
+        shift
+    done
+    
+    set -- "${args[@]}"
 
-#Enable systemd startup
-cp -r ${WORKDIR}/config/systemd/* /etc/systemd/system
-rm -f /etc/systemd/system/default.target
-ln -s /etc/systemd/system/custom.target /etc/systemd/system/default.target
+    start_install
+    exit 0
+}
 
-systemctl daemon-reload
+function start_install
+{
+    if [[ $EUID -ne 0 ]]; then
+       echo "This script must be run as root" 
+       exit 1
+    fi
+
+    #Install jq (bash json parser) library
+    if [[ ! -z "${NOUPDATE}" ]]; then
+        apt-get update
+    fi
+    apt-get install -y jq
+
+    #Put script in opt
+    WORKDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    mkdir -p /opt/gputweak
+    cp -r ${WORKDIR}/* /opt/gputweak
+    rm -rf /usr/bin/gputweak
+    ln -s /opt/gputweak/gputweak /usr/bin/gputweak
+
+    #Setup main xorg.conf
+    if [[ ! -z "/etc/X11/xorg.conf" ]]; then
+        NOW=$(date +%s)
+        chattr -i /etc/X11/xorg.conf
+        cp /etc/X11/xorg.conf "/etc/X11/xorg.conf.${NOW}"
+    fi
+    cp /opt/gputweak/config/xorg.conf /etc/X11/xorg.conf
+    chattr +i /etc/X11/xorg.conf
+
+    #Enable systemd startup
+    cp -r ${WORKDIR}/config/systemd/* /etc/systemd/system
+    rm -f /etc/systemd/system/default.target
+    ln -s /etc/systemd/system/custom.target /etc/systemd/system/default.target
+
+    systemctl daemon-reload
+}
