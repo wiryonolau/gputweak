fc4932bd2df189c7b412f8c2fcc445d236daea1b
Wiryono Lauw <wiryonolau@gmail.com>
2018-06-19 10:30:31 +0700

    fix amd tweak and install script

--- install.sh
+++ install.sh
@@ -10,15 +10,21 @@ apt-get install -y jq
 
 #Put script in opt
 WORKDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cp -r "${WORKDIR}/*" /opt/gputweak
+mkdir -p /opt/gputweak
+cp -r ${WORKDIR}/* /opt/gputweak
+rm -rf /usr/bin/gputweak
 ln -s /opt/gputweak/gputweak /usr/bin/gputweak
 
 #Setup main xorg.conf
+if [[ ! -z "/etc/X11/xorg.conf" ]]; then
+    chattr -i /etc/X11/xorg.conf
+    cp /etc/X11/xorg.conf /etc/X11/xorg.conf.orig
+fi
 cp /opt/gputweak/config/xorg.conf /etc/X11/xorg.conf
 chattr +i /etc/X11/xorg.conf
 
 #Enable systemd startup
-cp -r "${WORKDIR}/config/systemd/*" /etc/systemd/system
+cp -r ${WORKDIR}/config/systemd/* /etc/systemd/system
 rm -f /etc/systemd/system/default.target
 ln -s /etc/systemd/system/custom.target /etc/systemd/system/default.target
 
